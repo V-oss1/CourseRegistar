46008d3f632bb3819997439d4ae6511cc225907c
V-oss1 <demetrius07hardy@gmail.com>
2026-03-25 10:02:17 -0500

    Started on the CourseInfo.java file, or formatting the courses page.

--- src/Course.java
+++ src/Course.java
@@ -4,11 +4,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Course {
+public class Course { //Reads the courses
     protected String ID; //represents the ID of a course
     protected String name; //represents the name of a course
 
-    FileReader reader = new FileReader("CourseList.txt");
+    //FileReader reader = new FileReader("CourseList.txt");
     CourseInfo courses1 = new CourseInfo();
 
     public Course() throws IOException {
