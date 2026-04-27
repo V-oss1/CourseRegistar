5f26cffee4e72c9902bc606e2e5bffddc69a886d
Jose <128351563+jjosxs@users.noreply.github.com>
2026-04-27 17:40:52 -0500

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,45 +1,55 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+public class Course{
+    private String id; // format CSXXX
+    private String name;
+    private int registeredStudents; 
+    private int maxSeats;
+    private String[] prerequisites; // separated by comma 
 
-public class Course { //Reads the courses
-    protected String ID; //represents the ID of a course
-    protected String name; //represents the name of a course
+    // Constructor to initialize from file data 
+    public Course(String id, String name, int registered, int max, String[] prereqs) {
+        this.id = id;
+        this.name = name;
+        this.registeredStudents = registered;
+        this.maxSeats = max;
+        this.prerequisites = prereqs;
+    }
+    // Getters
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String[] getPrerequisites() {
+        return prerequisites;
+    }
 
-    //FileReader reader = new FileReader("CourseList.txt");
-    CourseInfo courses1 = new CourseInfo();
+    public int getRegisteredStudents() {
+        return registeredStudents;
+    }
 
-    public Course() throws IOException {
+    public int getMaxSeats() {
+        return maxSeats;
+    }
 
+
+    // check for open seats helper method
+    public boolean hasOpenSeats() {
+        return registeredStudents < maxSeats;
+    }
+
+    // increae registered student count by 1
+    public void increaseRegisteredStudents() {
+        if (hasOpenSeats()) {
+            registeredStudents++;
+        }
+    } 
+
+// returns status like 35/40
+public String getStatus() {
+    return registeredStudents + "/" + maxSeats;
+}
+    public String toString() {
+        return id + " - " + name + " (" + getStatus() + ")";
     }
 }
-
-
-// public class Course{
-//     private String id; // format CSXXX
-//     private String name;
-//     private int registeredStudents; 
-//     private int maxSeats;
-//     private String[] prerequisites; // separated by comma 
-
-//     // Constructor to initialize from file data 
-//     public Course(String id, String name, int registered, int max, String[] prereqs) {
-//         this.id = id;
-//         this.name = name;
-//         this.registeredStudents = registered;
-//         this.maxSeats = max;
-//         this.prerequisites = prereqs;
-//     }
-
-//     // sort by course ID method
-//     public int compareTo(Course other) {
-//         return this.id.compareTo(other.id);
-//     }
-
-//     // check for open seats helper method
-//     public boolean hasOpenSeats() {
-//         return registeredStudents < maxSeats;
-//     }
-// }
